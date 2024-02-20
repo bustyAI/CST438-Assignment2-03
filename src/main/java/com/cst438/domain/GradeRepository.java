54d30132c6e82d9877256e3e057453f89f3ea393
David Wisneski <dwisneski@csumb.edu>
2024-02-20 11:33:21 -0800

    file cleanup

--- src/main/java/com/cst438/domain/GradeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cst438.domain;
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GradeRepository extends CrudRepository<Grade, Integer> {
-    @Query("select g from Grade g where g.assignment.assignmentId=:assignmentId and g.enrollment.enrollmentId=:enrollmentId")
-    Grade findByEnrollmentIdAndAssignmentId(int enrollmentId, int assignmentId);
-}
