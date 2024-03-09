e0232a05a983b4e94369c64cac8cf0a8cbf776c7
Severin L. <severinlight3@gmail.com>
2024-03-08 16:57:07 -0800

    Implement StudentController, potential issue with userId and studentId

--- src/main/java/com/cst438/controller/StudentController.java
+++ src/main/java/com/cst438/controller/StudentController.java
@@ -2,11 +2,15 @@ package com.cst438.controller;
 
 import com.cst438.domain.*;
 import com.cst438.dto.EnrollmentDTO;
+import com.cst438.dto.SectionDTO;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -16,66 +20,175 @@ import java.util.Optional;
 @CrossOrigin(origins = "http://localhost:3000")
 public class StudentController {
 
+    @Autowired
+    EnrollmentRepository enrollmentRepository;
 
-   // student gets transcript showing list of all enrollments
-   // studentId will be temporary until Login security is implemented
-   //example URL  /transcript?studentId=19803
-   @GetMapping("/transcripts")
-   public List<EnrollmentDTO> getTranscript(@RequestParam("studentId") int studentId) {
+    @Autowired
+    TermRepository termRepository;
 
-       // TODO
+    @Autowired
+    UserRepository userRepository;
 
-       // list course_id, sec_id, title, credit, grade in chronological order
-       // user must be a student
-	   // hint: use enrollment repository method findEnrollmentByStudentIdOrderByTermId
-       // remove the following line when done
-       return null;
-   }
+    @Autowired
+    SectionRepository sectionRepository;
+
+    // student gets transcript showing list of all enrollments
+    // studentId will be temporary until Login security is implemented
+    // example URL /transcript?studentId=19803
+    @GetMapping("/transcripts")
+    public List<EnrollmentDTO> getTranscript(@RequestParam("studentId") int studentId) {
+        // TODO
+        // list course_id, sec_id, title, credit, grade in chronological order
+        // user must be a student
+        // hint: use enrollment repository method findEnrollmentByStudentIdOrderByTermId
+
+        List<Enrollment> enrollments = enrollmentRepository.findEnrollmentsByStudentIdOrderByTermId(studentId);
+        if (enrollments.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "No enrollments found for student Id: " + studentId);
+        }
+        List<EnrollmentDTO> elist = new ArrayList<>();
+        for (Enrollment enrollment : enrollments) {
+            Enrollment grade = null;
+            if (enrollment.getFinalGrade() !=null) {
+                grade = enrollmentRepository.(enrollment.getFinalGrade());
+            }
+            elist.add(new EnrollmentDTO(
+                    enrollment.getEnrollmentId(),
+                    enrollment.getFinalGrade(),
+                    enrollment.getUser().getId(),
+                    enrollment.getUser().getName(),
+                    enrollment.getUser().getEmail(),
+                    enrollment.getSection().getCourse().getCourseId(),
+                    enrollment.getSection().getSecId(),
+                    enrollment.getSection().getSectionNo(),
+                    enrollment.getSection().getBuilding(),
+                    enrollment.getSection().getRoom(),
+                    enrollment.getSection().getTimes(),
+                    enrollment.getSection().getCourse().getCredits(),
+                    enrollment.getSection().getTerm().getYear(),
+                    enrollment.getSection().getTerm().getSemester()));
+        }
+        return elist;
+    }
 
     // student gets a list of their enrollments for the given year, semester
     // user must be student
     // studentId will be temporary until Login security is implemented
-   @GetMapping("/enrollments")
-   public List<EnrollmentDTO> getSchedule(
-           @RequestParam("year") int year,
-           @RequestParam("semester") String semester,
-           @RequestParam("studentId") int studentId) {
+    @GetMapping("/enrollments")
+    public List<EnrollmentDTO> getSchedule(
+            @RequestParam("year") int year,
+            @RequestParam("semester") String semester,
+            @RequestParam("studentId") int studentId) {
 
+        // TODO
+        // hint: use enrollment repository method findByYearAndSemesterOrderByCourseId
 
-     // TODO
-	 //  hint: use enrollment repository method findByYearAndSemesterOrderByCourseId
-     //  remove the following line when done
-       return null;
-   }
-
+        List<Enrollment> enrollments = enrollmentRepository.findByYearAndSemesterOrderByCourseId(year, semester,
+                studentId);
+        if (enrollments.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "No enrollments found for student Id: " + studentId);
+        }
+        List<EnrollmentDTO> elist = new ArrayList<>();
+        for (Enrollment enrollment : enrollments) {
+            elist.add(new EnrollmentDTO(
+                    enrollment.getEnrollmentId(),
+                    enrollment.getFinalGrade(),
+                    enrollment.getUser().getId(),
+                    enrollment.getUser().getName(),
+                    enrollment.getUser().getEmail(),
+                    enrollment.getSection().getCourse().getCourseId(),
+                    enrollment.getSection().getSecId(),
+                    enrollment.getSection().getSectionNo(),
+                    enrollment.getSection().getBuilding(),
+                    enrollment.getSection().getRoom(),
+                    enrollment.getSection().getTimes(),
+                    enrollment.getSection().getCourse().getCredits(),
+                    enrollment.getSection().getTerm().getYear(),
+                    enrollment.getSection().getTerm().getSemester()));
+        }
+        return elist;
+    }
 
     // student adds enrollment into a section
     // user must be student
     // return EnrollmentDTO with enrollmentId generated by database
     @PostMapping("/enrollments/sections/{sectionNo}")
     public EnrollmentDTO addCourse(
-		    @PathVariable int sectionNo,
-            @RequestParam("studentId") int studentId ) {
+            @PathVariable int sectionNo,
+            @RequestParam("studentId") int studentId) {
 
         // TODO
-
         // check that the Section entity with primary key sectionNo exists
         // check that today is between addDate and addDeadline for the section
         // check that student is not already enrolled into this section
-        // create a new enrollment entity and save.  The enrollment grade will
+        // create a new enrollment entity and save. The enrollment grade will
         // be NULL until instructor enters final grades for the course.
 
-        // remove the following line when done.
-        return null;
+        Section section = sectionRepository.findById(sectionNo).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Section not found matching: " + sectionNo));
+
+        User student = userRepository.findById(studentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "User not found with student Id: " + studentId));
+
+        LocalDate today = LocalDate.now();
+        LocalDate localAddDate = (section.getTerm().getAddDate()).toLocalDate();
+        LocalDate localDeadline = (section.getTerm().getAddDeadline()).toLocalDate();
+
+        if (today.isBefore(localAddDate) || today.isAfter(localDeadline)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Unable to enroll. Enrollment period has ended for: " + sectionNo);
+        }
+
+        Enrollment existingEnrollment = enrollmentRepository.findEnrollmentBySectionNoAndStudentId(sectionNo,
+                studentId);
+        if (existingEnrollment != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Student is already enrolled in: " + sectionNo);
+        }
+
+        Enrollment enrollment = new Enrollment();
+        enrollment.setFinalGrade(null);
+        enrollment.setSection(section);
+
+        Enrollment savedEnrollment = enrollmentRepository.save(enrollment);
+
+        return new EnrollmentDTO(
+                savedEnrollment.getEnrollmentId(),
+                null,
+                studentId,
+                student.getName(),
+                student.getEmail(),
+                section.getCourse().getCourseId(),
+                section.getSecId(),
+                section.getSectionNo(),
+                section.getBuilding(),
+                section.getRoom(),
+                section.getTimes(),
+                section.getCourse().getCredits(),
+                section.getTerm().getYear(),
+                section.getTerm().getSemester());
 
     }
 
     // student drops a course
     // user must be student
-   @DeleteMapping("/enrollments/{enrollmentId}")
-   public void dropCourse(@PathVariable("enrollmentId") int enrollmentId) {
+    @DeleteMapping("/enrollments/{enrollmentId}")
+    public void dropCourse(@PathVariable("enrollmentId") int enrollmentId) {
+        // TODO
+        // check that today is not after the dropDeadline for section
 
-       // TODO
-       // check that today is not after the dropDeadline for section
-   }
+        Enrollment e = enrollmentRepository.findById(enrollmentId).orElse(null);
+        if (e != null && e.getUser().getType() == "student") {
+            LocalDate localDate = LocalDate.now();
+            Date today = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+            Date dropDeadline = e.getSection().getTerm().getDropDeadline();
+            if (!today.after(dropDeadline)) {
+                enrollmentRepository.delete(e);
+            } else {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "drop deadline has passed");
+            }
+        }
+    }
 }
