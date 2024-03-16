ea4da63369d8acf2cb2b463e9fd1b1576df89254
Severin L. <severinlight3@gmail.com>
2024-03-16 11:10:49 -0700

    Implement Transcript

--- src/components/student/Transcript.js
+++ src/components/student/Transcript.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+import { SERVER_URL } from "../../Constants";
+
 
 // students gets a list of all courses taken and grades
 // use the URL /transcript?studentId=
@@ -7,11 +9,53 @@ import React, {useState, useEffect} from 'react';
 //  Year, Semester, CourseId, SectionId, Title, Credits, Grade
 
 const Transcript = (props) => {
-     
-    return(
-        <> 
-            <h3>Not implemented</h3>
-        </>
+    const headers = ["year", "semester", "courseId", "sectionId", "credits", "grade"];
+    const [enrollments, getEnrollments] = useState([]);
+
+    useEffect(() => {
+        fetchTranscript();
+    }, []);
+
+    const fetchTranscript = async () => {
+        try {
+            // studentId=3 will be removed in assignment 7
+            const response = await fetch(`${SERVER_URL}/transcript?studentId=${3}`);
+            if (response.ok) {
+                const data = await response.json();
+                getEnrollments(data);
+            } else {
+                throw new Error('Failed to fetch transcript data');
+            }
+        } catch (error) {
+            console.error('Error fetching transcript data:', error);
+        }
+    };
+
+    return (
+      <>
+        <h3>Transcript</h3>
+        <table className="Center">
+          <thead>
+            <tr>
+              {headers.map((t, idx) => (
+                <th key={idx}>{t}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {enrollments.map((t) => (
+              <tr key={t.courseId}>
+                <td>{t.year}</td>
+                <td>{t.semester}</td>
+                <td>{t.courseId}</td>
+                <td>{t.sectionId}</td>
+                <td>{t.credits}</td>
+                <td>{t.grade}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
     );
 }
 
